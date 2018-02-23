34d0598637e26b9b985cbec6ed0bb3965990340a
Ryan Wiemer <ryan@ryanwiemer.com>
2018-02-23 14:53:48 -0800

    Remove siteConfig variables from 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,6 @@ import styled from 'styled-components'
 const Wrapper = styled.section`
   padding: 3em 1.5em;
   margin: 0 auto;
-  max-width: ${props => props.theme.sizes.maxWidthCentered};
 `;
 
 const Title = styled.h1`
@@ -19,21 +18,12 @@ const Title = styled.h1`
 const Text = styled.p`
   text-align: center;
   line-height: 1.6;
-
-  a {
-    transition: all .2s;
-    color: ${props => props.theme.colors.base};
-    &:hover {
-      color: ${props => props.theme.colors.highlight};
-    }
-  }
-
 `;
 
 const NotFoundPage = () => (
   <Wrapper>
     <Title>Error 404</Title>
-    <Text>Sorry, that page can't be found. Please return <Link to="/">home</Link>.</Text>
+    <Text>Sorry, that page can't be found</Text>
   </Wrapper>
 )
 

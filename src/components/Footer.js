fd9ce0f8c70d2226e6391ec82d08b24136e4b4da
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-05 01:25:26 -0700

    rel=“noopener” security fix
    See why: https://mathiasbynens.github.io/rel-noopener/

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,7 +45,11 @@ const Footer = () => (
   <Wrapper>
     <List>
       <Item>
-        <a href="https://www.contentful.com/" rel="nofollow" target="_blank">
+        <a
+          href="https://www.contentful.com/"
+          rel="nofollow noopener noreferrer"
+          target="_blank"
+        >
           <img
             src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
             style={{ width: '100px' }}
@@ -57,11 +61,16 @@ const Footer = () => (
         <a
           href="https://github.com/ryanwiemer/gatsby-starter-gcn"
           target="_blank"
+          rel="noopener noreferrer"
         >
           gatsby-starter-gcn
         </a>{' '}
         by{' '}
-        <a href="https://github.com/ryanwiemer" target="_blank">
+        <a
+          href="https://github.com/ryanwiemer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           @ryanwiemer
         </a>
       </Item>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,26 @@ const Index = ({ data }) => {
   return (
     <Container>
       <PageTitle small>
-        <a href="https://www.gatsbyjs.org/" target="_blank">
+        <a
+          href="https://www.gatsbyjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Gatsby
         </a>,{' '}
-        <a href="https://www.contentful.com/" target="_blank">
+        <a
+          href="https://www.contentful.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Contentful
         </a>{' '}
         and{' '}
-        <a href="https://www.netlify.com/" target="_blank">
+        <a
+          href="https://www.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Netlify
         </a>{' '}
         <span>🎉</span>

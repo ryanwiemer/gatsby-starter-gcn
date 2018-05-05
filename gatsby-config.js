ac24614becaac8f9154bf24c08c9ce57fe07f50a
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-05 02:18:10 -0700

    Clear unused var

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,23 @@
-require('dotenv').config();
-const config = require('./src/utils/siteConfig');
+require('dotenv').config()
+const config = require('./src/utils/siteConfig')
 
 // If the contentfulConfig can't be found assume the site is being built via Netlify with production environment variables
-try { var contentfulConfig = require('./.contentful');}
-catch (e) {
+try {
+  var contentfulConfig = require('./.contentful')
+} catch (e) {
+  // eslint-disable-next-line
   var contentfulConfig = {
-    "production": {
-      "spaceId": process.env.SPACE_ID,
-      "accessToken": process.env.ACCESS_TOKEN
-    }
+    production: {
+      spaceId: process.env.SPACE_ID,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
   }
 }
 
 module.exports = {
   plugins: [
     {
-    resolve: 'gatsby-plugin-canonical-urls',
+      resolve: 'gatsby-plugin-canonical-urls',
       options: {
         siteUrl: config.siteUrl,
       },
@@ -23,7 +25,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
-    resolve: `gatsby-transformer-remark`,
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
@@ -33,11 +35,12 @@ module.exports = {
       },
     },
     {
-    resolve: 'gatsby-source-contentful',
-      options: process.env.NODE_ENV === 'development' ?
-        contentfulConfig.development :
-        contentfulConfig.production
+      resolve: 'gatsby-source-contentful',
+      options:
+        process.env.NODE_ENV === 'development'
+          ? contentfulConfig.development
+          : contentfulConfig.production,
     },
-    'gatsby-plugin-netlify'
+    'gatsby-plugin-netlify',
   ],
 }

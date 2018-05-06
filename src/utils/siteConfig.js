e0585648799a9a895da02661a09b180221287c96
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-05 23:07:39 -0700

    PWA Manifest Config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteTitle: 'GCN',
+  shortTitle: 'GCN', // Used for App manifest e.g. Mobile Home Screen
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
   siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   copyright: 'Copyright © 2018. GCN User', // Copyright string for the RSS feed. Could also go in the footer.
   userName: 'GCN User', // Username for RSS author segment.
+  backgroundColor: '#e9e9e9',
+  themeColor: '#121212',
 }

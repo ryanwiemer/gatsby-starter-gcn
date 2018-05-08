2ba784f737d8d59b3d49b379aab5b2874bbd009c
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-08 16:04:54 -0700

    Contact page now uses SEO component
    Because the contact page is not defined in contentful, I modified the SEO component to allow a customTitle to specified. This way a custom title can be passed to the SEO component and meta tags can stay in the SEO component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,15 +4,19 @@ import config from '../utils/siteConfig'
 import Container from '../components/Container'
 import PageTitle from '../components/PageTitle'
 import ContactForm from '../components/ContactForm'
+import SEO from '../components/SEO'
 
 const Contact = ({ data }) => {
+  const postNode = {
+    title: `Contact - ${config.siteTitle}`,
+  }
+
   return (
     <div>
       <Helmet>
         <title>{`Contact - ${config.siteTitle}`}</title>
-        <meta property="og:title" content={`Contact - ${config.siteTitle}`} />
-        <meta property="og:url" content={`${config.siteUrl}/contact/`} />
       </Helmet>
+      <SEO postNode={postNode} pagePath="contact" customTitle />
 
       <Container>
         <PageTitle>Contact</PageTitle>

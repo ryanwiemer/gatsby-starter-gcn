769e8c58000cb9d8fc9f2370d3e5f9c9eb732639
Brandon Kalinowski <bwebservice@gmail.com>
2018-05-08 14:01:42 -0700

    Update Site Config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter Alt', // This allows an alternative site title for SEO schema.
-  shortTitle: 'shortGCN', // Used for App manifest e.g. Mobile Home Screen
+  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
+  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
   publisher: 'Publisher named GCN', // Organization name used for SEO schema
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
   siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and manifest.
   author: 'GCN User', // Author for RSS author segment and SEO schema.
-  authorUrl: 'https://gcn.netlify.com/author/', // URL used for author and publisher, can be social profile or other personal site
+  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@twitter', // For Twitter Cards
-  backgroundColor: '#e9e9e9',
-  themeColor: '#121212',
-  googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
-  copyright: 'Copyright © 2018. GCN User', // Copyright string for the RSS feed. Could also go in the footer.
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  googleAnalyticsID: '', // GA tracking ID.
+  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed. Could also go in the footer.
 }

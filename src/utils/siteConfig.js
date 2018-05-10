1db4bc3aa5d22926afcc984956e13f32150acdcb
Brandon Kalinowski <4714862+brandonkal@users.noreply.github.com>
2018-05-10 14:07:08 -0700

    RSS, PWA, Sitemap and Sticky Footer Fix (#10)
    RSS, PWA, Sitemap and Sticky Footer enhancements

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteTitle: 'GCN',
+
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not inculde a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+
+  author: 'GCN User', // Author for RSS author segment and SEO schema.
+
+  // PWA and Offline Manifest Settings
+  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and manifest.
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+
+  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed. Could also go in the footer.
 }

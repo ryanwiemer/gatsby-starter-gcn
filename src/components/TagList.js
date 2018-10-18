c6bb0fcff9217d7cd5490b4aa845791c6807b9b5
Ryan Wiemer <ryan@ryanwiemer.com>
2018-10-17 17:24:48 -0700

    fix line wrapping on tags

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -3,19 +3,21 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 
 const List = styled.ul`
-  margin: 0 auto 2em auto;
+  width: 100%;
+  margin: 0 auto 1em auto;
   max-width: ${props => props.theme.sizes.maxWidthCentered};
 `
 
 const Tag = styled.li`
   display: inline-block;
+  margin: 0 0.25em 0.25em 0;
   a {
+    float: left;
     transition: 0.2s;
     background: ${props => props.theme.colors.tertiary};
     padding: 0.5em;
     border-radius: 2px;
     text-transform: capitalize;
-    margin: 0 0.5em 0 0;
     text-decoration: none;
     color: ${props => props.theme.colors.base};
     border: 1px solid ${props => props.theme.colors.secondary};

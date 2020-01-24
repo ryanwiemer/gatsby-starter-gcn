c9d6574cecf88e8b08db2d11ae88835e39a708ca
ryanwiemer <ryan@ryanwiemer.com>
2020-01-24 14:09:58 -0800

    Add schema customization necessary until #19674 is resolved.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,46 @@
 const config = require('./src/utils/siteConfig')
 const path = require(`path`)
 
+/*
+  https://github.com/gatsbyjs/gatsby/issues/19674
+
+  This portion of the schema customization will be
+  removed once the above issue is resolved and
+  gatsby-plugin-schema-snapshot can be handle
+  the full extent of the schema generation.
+*/
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const typeDefs = `
+    type contentfulPostBodyTextNode implements Node
+    @childOf(types: ["ContentfulPost"]) {
+      id: ID!
+    }
+    type contentfulPageMetaDescriptionTextNode implements Node
+    @childOf(types: ["ContentfulPage"]) {
+      id: ID!
+    }
+    type contentfulPageBodyTextNode implements Node
+    @childOf(types: ["ContentfulPage"]) {
+      id: ID!
+    }
+    type contentfulPostMetaDescriptionTextNode implements Node
+    @childOf(types: ["ContentfulPost"]) {
+      id: ID!
+    }
+    type MarkdownRemark implements Node
+    @childOf(types: [
+      "contentfulPostBodyTextNode",
+      "contentfulPostMetaDescriptionTextNode",
+      "contentfulPageBodyTextNode",
+      "contentfulPageMetaDescriptionTextNode"
+    ]) {
+      id: ID!
+    }
+  `
+  actions.createTypes(typeDefs)
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
